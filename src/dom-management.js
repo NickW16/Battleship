599e9bd09fe6fecd6ecf2d4fc84476bce8040984
NickW16 <171855220+NickW16@users.noreply.github.com>
2025-09-15 16:08:41 -0300

    add some backbone of a battleship game layout

--- src/dom-management.js
+++ src/dom-management.js
@@ -1,11 +1,30 @@
+// dom stuff
 const manageDOM = () => {
-    const divTest = document.createElement('div');
-    divTest.id = 'test';
-    divTest.textContent = 'Battleship Game Test';
-    document.body.appendChild(divTest);
+    const divTest = document.getElementById('title');
+    divTest.textContent = 'Battleship Game';
+
+    // get both gameboards:
+    const gameboards = document.querySelectorAll('.gameboard');
+
+    const gameTiles = () => {
+        gameboards.forEach(gameboard => {
+            // Set up grid layout
+            // this styling has to be here
+            gameboard.style.display = 'grid';
+            gameboard.style.gridTemplateColumns = 'repeat(10, 40px)';
+            gameboard.style.gridTemplateRows = 'repeat(10, 40px)';
+            gameboard.style.gap = '1px';
+            for(let i = 0; i < 100; i++) {
+                let tile = document.createElement('div');
+                tile.classList.add('board-tile');
+                // tile styling is on ./styles.css
+                gameboard.appendChild(tile);
+            }
+        });
+    };
+    
+    gameTiles();
 };
 
 module.exports = { manageDOM };
 
-// it won't run without this line:
-document.addEventListener('DOMContentLoaded', manageDOM);
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 import "./styles.css";
 const { manageDOM } = require('./dom-management');
 const { ship } = require('./ship');
+
+// it won't run without this line:
+document.addEventListener('DOMContentLoaded', manageDOM);
